564801dd6dd3256cd21f9c4b70a0c55e811dc9ad
me <me@stuffihavemade.com>
2010-12-15 20:38:18 -0500

    added some tests

--- WatchCommunication/ISimpliciTIDriver.cs
+++ WatchCommunication/ISimpliciTIDriver.cs
@@ -16,12 +16,16 @@ namespace WatchCommunication
         bool PortOpen { get; }
         bool OpenComPort(string portName);
         void CloseComPort();
+        /// <summary>
+        /// Returns an empty string if no COM port is found.
+        /// </summary>
         string GetComPortName();
         /// <summary>
         /// The CC1111 constantly outputs data. If no new packets have been 
         /// recieved, data will be set to 255. So, to make sure no data is
         /// missed, GetData must run constantly in a loop (polling, not 
-        /// interrupt based).
+        /// interrupt based). If cannot connect, 0 will be outputted instead
+        /// of 255.
         /// </summary>
         /// <param name="data"></param>
         /// <returns></returns>
---
+++ WatchCommunicationTests/AccessPointTest.cs
@@ -0,0 +1,79 @@
+﻿using WatchCommunication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Rhino.Mocks;
+
+namespace WatchCommunicationTests
+{
+
+
+    /// <summary>
+    ///This is a test class for AccessPointTest and is intended
+    ///to contain all AccessPointTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class AccessPointTest
+    {
+        private ISimpliciTIDriver mockSimplicitiDriver;
+        private AccessPoint accessPoint;
+
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            mockSimplicitiDriver = MockRepository.GenerateStub<ISimpliciTIDriver>();
+            accessPoint = new AccessPoint(mockSimplicitiDriver);
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(AccessPointException))]
+        public void CannotFindCOMPortTest()
+        {
+            mockSimplicitiDriver.Expect(m => m.PortOpen).Return(false);
+            mockSimplicitiDriver.Stub(m => m.GetComPortName()).Return("");
+            accessPoint.Open();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(AccessPointException))]
+        public void CannotOpenTest()
+        {
+            mockSimplicitiDriver.Expect(m => m.PortOpen).Return(false);
+            mockSimplicitiDriver.Stub(m => m.GetComPortName()).Return("COM0");
+            accessPoint.Open();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(AccessPointException))]
+        public void CannotStartSimpliciTITest()
+        {
+            mockSimplicitiDriver.Expect(m => m.PortOpen).Return(false);
+            mockSimplicitiDriver.Stub(m => m.GetComPortName()).Return("COM0");
+            mockSimplicitiDriver.Expect(m => m.StartSimpliciTI()).Return(false);
+            accessPoint.Open();
+        }
+
+        [TestMethod()]
+        public void IsConnectedTest()
+        {
+            mockSimplicitiDriver.Expect(m => m.PortOpen).Return(true);
+            accessPoint.Open();
+            Assert.AreEqual<bool>(accessPoint.IsConnected(), true);
+        }
+
+        //[TestMethod()]
+        //public void PacketRecievedTest()
+        //{
+        //    mockSimplicitiDriver.Expect(m => m.PortOpen).Return(true);
+
+        //    mockSimplicitiDriver.Stub(m => m.GetData(out 0)).IgnoreArguments().OutRef(1);
+        //    bool recieved = false;
+        //    accessPoint.OnPacketRecieved += (d) => { recieved = true; };
+        //    accessPoint.Open();
+        //    while (recieved == false) { };
+        //    Assert.AreEqual<bool>(true, true);
+        //}
+    }
+
+}
